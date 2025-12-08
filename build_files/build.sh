d7f270c46786368158a361ecab089bf7749a8fe9
Robert Ball <jhrobert@pm.me>
2025-12-07 22:14:52 -0800

    install just via dnf5

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,6 +10,12 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux neovim bat fish
+dnf5 install -y \
+  tmux \
+  neovim \
+  bat \
+  fish \
+  just
 
 systemctl enable podman.socket
+systemctl enable --now cockpit.service
